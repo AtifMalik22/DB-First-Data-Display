9a12652da3df8d2c7d3ea8a0407f0cbb66303229
atif javed <atihmalik2@gmail.com>
2024-07-10 10:37:20 +0500

    commit

--- Demo_Project/Services/EmailService.cs
+++ Demo_Project/Services/EmailService.cs
@@ -18,32 +18,6 @@ namespace Demo_Project.Services
             _webHostEnvironment = webHostEnvironment;
         }
 
-        //public async Task SendEmailWithPdfAsync(string toEmail, string subject, string body, string pdfContent)
-        //{
-        //    var emailConfig = _configuration.GetSection("EmailCred");
-        //    var smtpClient = new SmtpClient(emailConfig["SmtpHost"])
-        //    {
-        //        Port = 587,
-        //        Credentials = new NetworkCredential(emailConfig["UserName"], emailConfig["Password"]),
-        //        EnableSsl = true,
-        //    };
-
-        //    var mailMessage = new MailMessage
-        //    {
-        //        From = new MailAddress(emailConfig["UserName"]),
-        //        Subject = subject,
-        //        Body = body,
-        //        IsBodyHtml = true,
-        //    };
-
-        //    mailMessage.To.Add(toEmail);
-
-        //    // Generate PDF and attach it
-        //    var pdfStream = GeneratePdf(pdfContent);
-        //    mailMessage.Attachments.Add(new Attachment(pdfStream, "PayrollDetails.pdf", "application/pdf"));
-
-        //    await smtpClient.SendMailAsync(mailMessage);
-        //}
         public async Task SendEmailWithPdfAsync(string toEmail, string subject, string body, string pdfContent)
         {
             try
@@ -80,24 +54,6 @@ namespace Demo_Project.Services
             }
         }
 
-
-        //private MemoryStream GeneratePdf(string htmlContent)
-        //{
-        //    var stream = new MemoryStream();
-        //    var document = new Document(PageSize.A4, 25, 25, 30, 30);
-        //    PdfWriter writer = PdfWriter.GetInstance(document, stream);
-        //    writer.CloseStream = false;
-
-        //    document.Open();
-        //    using (var stringReader = new StringReader(htmlContent))
-        //    {
-        //        XMLWorkerHelper.GetInstance().ParseXHtml(writer, document, stringReader);
-        //    }
-        //    document.Close();
-        //    stream.Position = 0;
-
-        //    return stream;
-        //}
         private MemoryStream GeneratePdf(string htmlContent)
         {
             var stream = new MemoryStream();
